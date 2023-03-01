3c1c009442959ad6f867e20151c784d87ec15a97
qara-qurt <serikov.2002.12@gmail.com>
2023-03-01 22:55:43 +0600

    added little bit telegram logic working

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"pingrobot/models"
 	"time"
 )
@@ -9,6 +13,8 @@ import (
 const (
 	WOKER_COUNT = 3
 	INTERVAL    = time.Second * 5
+	TG_TOKEN    = "6040490988:AAFdeU7w_369vpt7iuwyfN4tqhK8wnUcHlM"
+	TG_CHAT_ID  = "958729521"
 )
 
 var urls = []string{
@@ -30,6 +36,9 @@ func main() {
 func getResponseInfo(result <-chan models.Result) {
 	for r := range result {
 		fmt.Println(r.Info())
+		if r.Error != nil {
+			SendMessage(r.Info())
+		}
 	}
 }
 
@@ -53,4 +62,34 @@ func worker(jobs <-chan models.URL, result chan<- models.Result) {
 	for job := range jobs {
 		result <- job.Process()
 	}
+}
+
+func getUrl() string {
+	return fmt.Sprintf("https://api.telegram.org/bot%s", TG_TOKEN)
+}
+
+func SendMessage(text string) error {
+	var err error
+	var response *http.Response
+
+	url := fmt.Sprintf("%s/sendMessage", getUrl())
+
+	body, _ := json.Marshal(map[string]string{
+		"chat_id": TG_CHAT_ID,
+		"text":    text,
+	})
+
+	response, err = http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	body, err = io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
